229048978452de43fe4a526728d27fcd5cab2180
ish <154955779+ibj21@users.noreply.github.com>
2024-07-18 13:21:14 +0530

    Add files via upload

--- StringMethod.java
+++ StringMethod.java
@@ -3,5 +3,6 @@ public static void main(String[]args){
 String txt="Hello World";
 System.out.println(txt.toUpperCase());
 System.out.println(txt.toLowerCase());
+System.out.println(txt.indexOf("World"));
 }
 }
