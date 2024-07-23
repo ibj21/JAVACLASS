576dbad2d4bc87da083a25fb58ed1f1649e6d774
ish <154955779+ibj21@users.noreply.github.com>
2024-07-23 12:44:59 +0530

    Add files via upload

--- ForLoop.java
+++ ForLoop.java
@@ -1,6 +1,10 @@
 public class ForLoop {
     public static void main(String[] args) {
-        for(int i=0;i<5 ; i++){
+        // for(int i=0;i<5 ; i++){
+        //     System.out.println(i);
+        // }
+        String[] cars={"a","b","c"};
+        for (String i : cars) {
             System.out.println(i);
         }
     }
