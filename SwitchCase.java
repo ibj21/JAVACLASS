8624ad23acb3f1c52d89d9a99131d6944c4d2792
ish <154955779+ibj21@users.noreply.github.com>
2024-07-23 12:21:39 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,11 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int day=4;
+        switch(day){
+            case 1: System.out.println("mon"); break;
+            case 2: System.out.println("tues"); break;
+            case 4: System.out.println("thurs"); break;
+            default: System.out.println("invalid");
+        }
+    }
+}
