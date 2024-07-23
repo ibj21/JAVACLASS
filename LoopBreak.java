0d511a16b0c28fa0b39c4f5ca78fe0651211db29
ish <154955779+ibj21@users.noreply.github.com>
2024-07-23 12:50:15 +0530

    Add files via upload

---
+++ LoopBreak.java
@@ -0,0 +1,9 @@
+public class LoopBreak {
+    public static void main(String[] args) {
+        for(int i=0; i< 10; i++){
+            if(i==4)
+            break;
+            System.out.println(i);
+        }
+    }
+}
