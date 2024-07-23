8fb151d6a15f9cf4e69e3d86ad3c9846f69231d6
ish <154955779+ibj21@users.noreply.github.com>
2024-07-23 12:57:21 +0530

    Add files via upload

---
+++ Inputs.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        String user;
+        System.out.println("enter username");
+        user=scan.nextLine();
+        System.out.println(user);
+    }
+}
