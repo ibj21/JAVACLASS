599f8083bb27f6490baff1ea94fa618f30b1067e
ish <154955779+ibj21@users.noreply.github.com>
2024-07-23 12:51:57 +0530

    Add files via upload

---
+++ LoopCont.java
@@ -0,0 +1,9 @@
+public class LoopCont {
+    public static void main(String[] args) {
+        for(int i=0; i< 10; i++){
+            if(i==4)
+            continue;
+            System.out.println(i);
+        }
+    }
+}
