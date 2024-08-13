3b2bc53a3c38d7ae6f92b3bc6678155b00861120
ish <154955779+ibj21@users.noreply.github.com>
2024-08-13 12:38:47 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
-public class Main{
-public static void main(String[]args){
-int myInt=9;
-double myDouble= myInt; //implicit type casting
-System.out.println(myInt);
-System.out.println(myDouble);
+class Animal {
+    public void animalSound(){
+        System.out.println("the animal makes a sound");
+    }
 }
+
+
+
+class Pig extends Animal {
+    public void animalSound(){
+        System.out.println("pig makes wee wee");
+    }
+}
+
+class Dog extends Animal {
+    public void animalSound(){
+        System.out.println("dog says bow wow");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Animal obj = new Animal();
+        Animal dog= new Dog();
+        Animal pig= new Pig();
+        obj.animalSound();
+        pig.animalSound();
+        dog.animalSound();
+    }
 }
